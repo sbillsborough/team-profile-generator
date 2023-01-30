00ad0cd8737182adb8995073bc0d48e508af461c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-30 12:09:58 +0000

    createManager function added that prompts for the managers information, validates that the user input is in the proper format and then pushes the created Manager class to the teamArray

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const promptUser = require('./lib/promptUser')
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+let teamArray = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Prompts the user for which type of employee they would like to add
 function promptUser() {
   inquirer.prompt([
     {
@@ -32,9 +33,76 @@ function promptUser() {
       createIntern();
     } else if (userInput.employeeRoll === 'None') {
       createTeam();
-    }
-  })
+    };
+  });
+};
+
+function createManager() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter manager\'s name.',
+      name: 'mngrName',
+      validate: (mngrName) => {
+        if (mngrName) {
+          return true;
+        }
+        else {
+          console.log('Enter manager\'s name.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: 'Enter manager\'s ID.',
+      name: 'mngrId',
+      validate: (mngrId) => {
+        if (mngrId) {
+          return true;
+        }
+        else {
+          console.log('Enter manager\'s name.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: 'Enter manager\'s email.',
+      name: 'mngrEmail',
+      validate: (mngrEmail) => {
+        if (mngrEmail) {
+          return true;
+        }
+        else {
+          console.log('Enter manager\'s email.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: 'Enter manager\'s office number.',
+      name: 'mngrOfficeNumber',
+      validate: (mngrOfficeNumber) => {
+        if (mngrOfficeNumber) {
+          return true;
+        }
+        else {
+          console.log('Enter manager\'s office number.');
+          return false;
+        }
+      }
+    },
+  ]).then((answers) => {
+    const Manager = new Manager(answers.mngrName, answers.mngrId, answers.mngrEmail, answers.mngrOfficeNumber);
+    teamArray.push(Manager);
+    run();
+  });
 }
+
+
 
 // function to initialize program
 function init() {
