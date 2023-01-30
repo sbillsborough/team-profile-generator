9ab36b5c3d0635bb457a649dee1e8a06eb9005a4
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-30 11:16:53 +0000

    moved the promptUser function into a promptUser.js file and required it into the index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const promptUser = require('./lib/promptUser')
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
